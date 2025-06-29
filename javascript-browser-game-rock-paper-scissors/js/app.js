974841d5f466ed4dabfd723cd528f85ad81b547e
alya <aliya2002mah@gmail.com>
2025-06-29 10:28:42 +0300

    finish scaffolding html and css

---
+++ javascript-browser-game-rock-paper-scissors/js/app.js
@@ -0,0 +1,19 @@
+/*-------------------------------- Constants --------------------------------*/
+const choices = ['rock', 'paper', 'scissors'];
+
+
+/*-------------------------------- Variables --------------------------------*/
+let playerChoice;
+let computerChoice; 
+let msg;
+
+
+/*------------------------ Cached Element References ------------------------*/
+const resultDisplayEl = document.querySelector('#result-display');
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
