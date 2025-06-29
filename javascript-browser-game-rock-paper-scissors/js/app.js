75fd0e35c69ab4012fc1e8da573c1c3a0260b951
alya <aliya2002mah@gmail.com>
2025-06-29 11:09:28 +0300

    Created play, render, getComputerChoice

--- javascript-browser-game-rock-paper-scissors/js/app.js
+++ javascript-browser-game-rock-paper-scissors/js/app.js
@@ -9,13 +9,35 @@ let msg;
 
 
 /*------------------------ Cached Element References ------------------------*/
-const resultDisplayEl = document.querySelector('#result-display');
 const rockBtnEl = document.querySelector('#rock');
 const paperBtnEl = document.querySelector('#paper');
 const scissorsBtnEl = document.querySelector('#scissors');
 
 /*-------------------------------- Functions --------------------------------*/
+play();
 
+function getComputerChoice ()
+{
+    // 1. generate a random number 0-2
+    const randomIndex = Math.floor(Math.random()*choices.length);
+    console.log('Random Index:', randomIndex);
+    // 2. select the item from the array
+   return choices[randomIndex];
+}
 
+function play ()
+{
+    computerChoice = getComputerChoice();
+    console.log('computer choice:' , computerChoice);
+
+    //after updating state, render to html
+    render();
+}
+
+//updates html directly
+function render ()
+{
+
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
